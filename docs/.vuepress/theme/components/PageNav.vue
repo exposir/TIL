0778e9d1112835ecde56421cae64d9591f8ea62a
menglingyu <menglingyu@taou.com>
2022-01-29 13:24:33 +0800

    update

--- docs/.vuepress/theme/components/PageNav.vue
+++ docs/.vuepress/theme/components/PageNav.vue
@@ -1,28 +1,16 @@
 <template>
   <div>
-    <PageNavOrigin :sidebarItems="sidebarItems"/>
-    <div class="icp-footer">
-      <p>Copyright© 2020-{{(new Date()).getFullYear()}} 卡颂</p>
-      <a href="https://beian.miit.gov.cn/" target="_blank">京ICP备20028082号-1</a>
-    </div>
+    <PageNavOrigin :sidebarItems="sidebarItems" />
   </div>
 </template>
-
 <script>
-import PageNavOrigin from '@vuepress/theme-default/components/PageNav.vue'
+import PageNavOrigin from "@vuepress/theme-default/components/PageNav.vue";
 
 export default {
-  name: 'PageNav',
-  props: ['sidebarItems'],
+  name: "PageNav",
+  props: ["sidebarItems"],
   components: {
-    PageNavOrigin
+    PageNavOrigin,
   },
-}
+};
 </script>
-
-<style>
-.icp-footer {
-  text-align: center;
-}
-
-</style>
