d4932a666676d283f5c1db1ce281d49d45f985fd
menglingyu <menglingyu@taou.com>
2022-02-09 20:18:03 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const sidebar = {
     {
       title: "文章",
       collapsable: true,
-      path: "/post/文章",
+      // path: "/post/文章",
       children: [
         ["/post/重学Javascript（一）", "重学Javascript（一）"],
         [
